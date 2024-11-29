ff74e3355980cc3140865dace660382fe849cf2d
Rzeigui Ahmed <ahmedrzeigui52@gmail.com>
2024-11-29 10:52:44 +0100

    Création de la structure du projet

---
+++ aipasswordcheck/src/main/java/com/f2/revue_code/App.java
@@ -0,0 +1,13 @@
+package com.f2.revue_code;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
